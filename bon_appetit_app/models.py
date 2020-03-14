aa2b52e10a8d45d5450bf72bcf31fee7df88270c
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2020-03-14 14:17:46 +0000

    restaurant model updates with slug

--- bon_appetit_app/models.py
+++ bon_appetit_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class City(models.Model):
     name = models.CharField(max_length=128, unique=True)
@@ -20,6 +21,12 @@ class Restaurant(models.Model):
     price = models.IntegerField(default=0)
     picture = models.ImageField(default=0)
     rating = models.IntegerField(default=0)
+
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Restaurant, self).save(*args, **kwargs)
     
     def __str__(self):
         return self.name
