9ae9cbec4a41bb38ef8edcde3b6d062fcdabd316
Daniel Kirkwood <2324926k@student.gla.ac.uk>
2020-03-19 13:52:19 +0000

    updated top restaurant page to filter correctly

--- bon_appetit_app/views.py
+++ bon_appetit_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Avg
+from django.db.models import Func
 from bon_appetit_app.models import Restaurant
 
 def home(request):
@@ -19,6 +21,14 @@ def searchResults(request):
 
 def topRestaurants(request):
     context = {}
+
+    rating_list = Restaurant.objects.annotate(avg_rating=Round(Avg('fooditem__rating'))).order_by('-avg_rating')[:6]
+    context['rating_list'] = rating_list
+
+    # get the 5 cheapest restaurants based on average price
+    cheapest_list = Restaurant.objects.annotate(avg_rating=Round(Avg('fooditem__rating')), avg_price=Avg('fooditem__price')).order_by('avg_price')[:6]
+
+    context['cheapest_list'] = cheapest_list
 
     # add 'active' to context dict so show current page as active
     context["top_restaurants_page"] = "active"
@@ -40,3 +50,7 @@ def viewAccount(request):
 
 def editAccount(request):
     pass
+
+class Round(Func):
+    function = 'ROUND'
+    template='%(function)s(%(expressions)s, 0)'
